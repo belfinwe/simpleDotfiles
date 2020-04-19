3ef0cc17dcd4a6b1fa08f0070fdfc6063a025481
Joakim Nilsen <joakimrn02@gmail.com>
2020-04-19 21:13:53 +0200

    Added else statement, related to issue #12. Now documents that which is not installed.

--- programs.sh
+++ programs.sh
@@ -11,64 +11,89 @@ today=$(date)
 # git
 # ##################################################
 sudo apt-get install git -y
-if [ -x "$(command -v git)" ];then
+if [ -x "$(command -v git)" ]
+then
     echo $today" - git installed!\n" >&2 >> installLog
+else
+    echo $today" - git NOT installed!\n" >&2 >> installLog
 fi
 
 # ##################################################
 # vim
 # ##################################################
 sudo apt-get install vim -y
-if [ -x "$(command -v vim)" ];then
+if [ -x "$(command -v vim)" ]
+then
     echo $today" - vim install!\n" >&2 >> installLog
+else
+    echo $today" - vim NOT install!\n" >&2 >> installLog
+
 fi
 
 # ##################################################
 # tty-clock
 # ##################################################
 sudo apt-get install tty-clock -y
-if [ -x "$(command -v tty-clock)" ]; then
+if [ -x "$(command -v tty-clock)" ]
+then
     echo $today' - tty-clock installed!\n' >&2 >> installLog
+else
+    echo $today' - tty-clock NOT installed!\n' >&2 >> installLog
 fi
 
 # ##################################################
 # dropbox
 # ##################################################
 sudo apt-get install dropbox -y
-if [ -x "$(command -v dropbox)" ]; then
+if [ -x "$(command -v dropbox)" ]
+then
     echo $today' - dropbox installed!\n' >&2 >> installLog
+else
+    echo $today' - dropbox NOT installed!\n' >&2 >> installLog
 fi
 
 # ##################################################
 # vlc
 # ##################################################
 sudo apt-get install vlc -y
-if [ -x "$(command -v vlc)" ]; then
+if [ -x "$(command -v vlc)" ]
+then
     echo $today' - vlc installed' >&2 >> installLog
+else
+    echo $today' - vlc NOT installed' >&2 >> installLog
 fi
 
 # ##################################################
 # neofetch
 # ##################################################
 sudo apt-get install neofetch -y
-if [ -x "$(command -v neofetch)" ]; then
+if [ -x "$(command -v neofetch)" ]
+then
     echo $today" - neofetch installed!\n" >&2 >> installLog
+else
+    echo $today" - neofetch NOT installed!\n" >&2 >> installLog
 fi
 
 # ##################################################
 # htop
 # ##################################################
 sudo apt-get install htop -y
-if [ -x "$(command -v htop)" ]; then
+if [ -x "$(command -v htop)" ]
+then
     echo $today" - htop installed!\n" >&2 >> installLog
+else
+    echo $today" - htop NOT installed!\n" >&2 >> installLog
 fi
 
 # ##################################################
 # tree
 # ##################################################
 sudo apt-get install tree -y
-if [ -x "$(command -v tree)" ]; then
+if [ -x "$(command -v tree)" ]
+then
     echo $today" - tree installed!\n" >&2 >> installLog
+else
+    echo $today" - tree NOT installed!\n" >&2 >> installLog
 fi
 
 
